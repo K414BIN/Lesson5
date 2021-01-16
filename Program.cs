56315bd47f1a57e3d746b3270aa20f91d48af524
Alexander Kalabin <k414bin@yandex.ru>
2021-01-16 10:50:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using static Less_5.ToDo;
+namespace Less_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Tasks.xml";
+            if (File.Exists(fName)) 
+            {
+       
+                 var serializer = new XmlSerializer(typeof(Task));
+                 Task result;
+
+                               using (TextReader reader = new StringReader(fName))
+                             {
+                               result = (Task)serializer.Deserialize(reader);
+                             }
+              
+                Console.WriteLine(result);
+                Console.ReadKey();
+        
+            }
+            else
+            {
+
+                var company = new ToDo();
+                company.duty = new List<Task>() { new Task() { Title = "Бегать по утрам.", Count = 1 ,isDone=false } };
+                SerializeToXml(company, fName);
+
+            }
+        }
+
+        public T DeserializeToObject<T>(string filepath) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+ 
+        public static void SerializeToXml<T>(T anyobject, string xmlFilePath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(anyobject.GetType());
+            using (StreamWriter writer = new StreamWriter(xmlFilePath))
+            {
+                xmlSerializer.Serialize(writer, anyobject);
+            }
+        }
+
+    }
+}
---
+++ ToDo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Less_5
+{
+    [XmlRoot(ElementName = "ToDo")]
+   public class ToDo
+    {
+        public ToDo()
+        {
+            duty = new List<Task>();
+        }
+        [XmlElement(ElementName = "Task")]
+        public List<Task> duty { get; set; }
+
+        public Task this[string name]
+        {
+            get { return duty.FirstOrDefault(s => string.Equals(s.Title, name, StringComparison.OrdinalIgnoreCase)); }
+        }
+}
+   public class Task
+    {
+        [XmlAttribute("count")]
+        public byte Count { get; set; }
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+        [XmlAttribute("isdone")]
+        public bool isDone { get; set; }
+    }
+}
